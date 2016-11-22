cfe4cfe45ac985addc94e27aa67f4232870c189f
Tomisin <tomisinjenrola@gmail.com>
2016-11-22 19:26:37 -0330

    Implemented 'add player' and 'remove player' functionality

--- Source_code/GUI/RegisterTeam.java
+++ Source_code/GUI/RegisterTeam.java
@@ -1,6 +1,5 @@
 package GUI;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -9,19 +8,158 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JSeparator;
-import javax.swing.JScrollBar;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import Model.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 public class RegisterTeam extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField teamNameTextField;
-	private JTextField playerNameTextField;
-	private JTextField textField;
-
+	private JTextField txtTeamName;
+	private JTextField txtPlayerName;
+	private JTextField txtPlayerAge;
+	private ArrayList<Player> players;
+	
 	/**
-	 * Launch the application.
+	 * Create the frame.
+	 */
+	public RegisterTeam() {
+		setTitle("Register team");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 454, 374);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		txtTeamName = new JTextField();
+		txtTeamName.setBounds(101, 27, 313, 19);
+		contentPane.add(txtTeamName);
+		txtTeamName.setColumns(10);
+
+		JLabel lblTeamName = new JLabel("Team Name:");
+		lblTeamName.setBounds(14, 30, 85, 15);
+		contentPane.add(lblTeamName);
+
+		JLabel lblPlayerDetails = new JLabel("Player Details");
+		lblPlayerDetails.setBounds(14, 107, 132, 15);
+		contentPane.add(lblPlayerDetails);
+
+		txtPlayerName = new JTextField();
+		txtPlayerName.setBounds(101, 133, 114, 19);
+		contentPane.add(txtPlayerName);
+		txtPlayerName.setColumns(10);
+
+		JLabel lblPlayerName = new JLabel("Name:");
+		lblPlayerName.setBounds(14, 136, 70, 15);
+		contentPane.add(lblPlayerName);
+
+		JLabel lblPlayerAge = new JLabel("Age:");
+		lblPlayerAge.setBounds(14, 166, 70, 15);
+		contentPane.add(lblPlayerAge);
+
+		txtPlayerAge = new JTextField();
+		txtPlayerAge.setBounds(101, 163, 114, 19);
+		contentPane.add(txtPlayerAge);
+		txtPlayerAge.setColumns(10);
+
+		JComboBox cmbTournament = new JComboBox();
+		cmbTournament.setBounds(101, 57, 313, 24);		
+		contentPane.add(cmbTournament);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowOpened(WindowEvent arg0) {
+				//ADD TOURNAMENTS TO COMBO BOX
+				players = new ArrayList<Player>();
+			}
+		});
+		
+		JButton btnRegisterTeam = new JButton("Register Team");
+		btnRegisterTeam.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent arg0) {
+				
+				String name = txtTeamName.getText();
+				Team team = new Team(name);
+				
+				for (Player p : players) {
+					team.addPlayer(p);
+				}
+				String tournamentName = cmbTournament.getSelectedItem().toString();
+				
+				//Loop through list of tournaments to find correct one
+				//Try to add team to tournament 
+				
+			}
+		});
+		btnRegisterTeam.setBounds(14, 301, 121, 23);
+		contentPane.add(btnRegisterTeam);
+		
+		JList<String> lstPlayers = new JList<String>();
+		lstPlayers.setBounds(228, 107, 186, 169);
+		contentPane.add(lstPlayers);
+		DefaultListModel<String> model = new DefaultListModel<String>();
+		lstPlayers.setModel(model);
+		
+		JButton btnAddPlayer = new JButton("Add Player");
+		btnAddPlayer.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String name = txtPlayerName.getText();
+				int age = 0;
+				try {
+					age = Integer.parseInt(txtPlayerAge.getText());
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "Please enter valid player information", "Error", JOptionPane.ERROR_MESSAGE);
+					txtPlayerName.setText("");
+					txtPlayerAge.setText("");
+					return;
+				}
+				Player p = new 	Player(name, age);
+				players.add(p);
+				model.addElement(name + " : " + age);
+				txtPlayerName.setText("");
+				txtPlayerAge.setText("");
+			}
+		});
+		btnAddPlayer.setBounds(101, 194, 114, 23);
+		contentPane.add(btnAddPlayer);
+		
+		JButton btnRemovePlayer = new JButton("Remove Player");
+		btnRemovePlayer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String name = lstPlayers.getSelectedValue();
+				name = name.substring(0, name.indexOf(':') - 1);
+				
+				for (Player p : players) 
+					if (p.getName() == name)
+						players.remove(p);
+				
+				
+				model.remove(lstPlayers.getSelectedIndex());
+				
+			}
+		});
+		btnRemovePlayer.setBounds(228, 301, 186, 23);
+		contentPane.add(btnRemovePlayer);
+		
+		JLabel lblTournament = new JLabel("Tournament:");
+		lblTournament.setBounds(14, 62, 85, 14);
+		contentPane.add(lblTournament);
+	
+	}
+	
+	/**
+	 * Test Frame
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -35,57 +173,5 @@ public class RegisterTeam extends JFrame {
 				}
 			}
 		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public RegisterTeam() {
-		setTitle("Register team");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 454, 374);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-		teamNameTextField = new JTextField();
-		teamNameTextField.setBounds(84, 61, 114, 19);
-		contentPane.add(teamNameTextField);
-		teamNameTextField.setColumns(10);
-
-		JLabel teamNameLabel = new JLabel("Name:");
-		teamNameLabel.setBounds(12, 63, 70, 15);
-		contentPane.add(teamNameLabel);
-
-		JList list = new JList();
-		list.setBounds(259, 50, 125, 142);
-		contentPane.add(list);
-
-		JLabel lblPlayerDetails = new JLabel("Player Details");
-		lblPlayerDetails.setBounds(12, 140, 186, 15);
-		contentPane.add(lblPlayerDetails);
-
-		playerNameTextField = new JTextField();
-		playerNameTextField.setBounds(84, 173, 114, 19);
-		contentPane.add(playerNameTextField);
-		playerNameTextField.setColumns(10);
-
-		JLabel playerNameLabel = new JLabel("Name:");
-		playerNameLabel.setBounds(12, 177, 70, 15);
-		contentPane.add(playerNameLabel);
-
-		JLabel lblAge = new JLabel("Age:");
-		lblAge.setBounds(12, 212, 70, 15);
-		contentPane.add(lblAge);
-
-		textField = new JTextField();
-		textField.setBounds(84, 210, 114, 19);
-		contentPane.add(textField);
-		textField.setColumns(10);
-
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(12, 259, 186, 24);
-		contentPane.add(comboBox);
 	}
 }
