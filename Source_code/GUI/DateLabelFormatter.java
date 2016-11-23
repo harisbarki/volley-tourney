46998db92961977dbd06e59f4cce4aab9393c2e5
Kelwin Joanes <kjoanes@gmail.com>
2016-11-23 09:10:44 -0330

    Moved the DateLabelFormatter class to GUI package

---
+++ Source_code/GUI/DateLabelFormatter.java
@@ -0,0 +1,32 @@
+package GUI;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import java.text.ParseException;
+
+
+public class DateLabelFormatter extends AbstractFormatter
+{
+    private String datePattern = "dd/MM/yyyy";
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+    @Override
+    public Object stringToValue(String text) throws ParseException 
+    {
+        return dateFormatter.parseObject(text);
+    }
+
+    @Override
+    public String valueToString(Object value) throws ParseException
+    {
+        if (value != null)
+        {
+            Calendar cal = (Calendar) value;
+            return dateFormatter.format(cal.getTime());
+        }
+
+        return "";
+    }
+
+}
