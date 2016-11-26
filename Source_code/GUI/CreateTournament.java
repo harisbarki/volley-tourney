3c0c7e1b5f20f3683be533dae3dc808fd305f2e2
Haris Barki <harisbarki@ymail.com>
2016-11-26 11:49:01 -0330

    fixed file paths

--- Source_code/GUI/CreateTournament.java
+++ Source_code/GUI/CreateTournament.java
@@ -50,6 +50,7 @@ public class CreateTournament extends JFrame implements ActionListener
 	// instance variables
 	private final int WIDTH  = 550;
 	private final int HEIGHT = 600;
+	private final String tournamentFile = "src/tournaments/files.txt";
 
 	private JLabel tNameLbl;
 	private JLabel tStartDateLbl;
@@ -552,14 +553,14 @@ public class CreateTournament extends JFrame implements ActionListener
 				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 				
 				// configure save path of current file
-				fileName = "tournaments/" + fileName + ".xml";
+				fileName = "src/tournaments/" + fileName + ".xml";
 				
 				// save name of each file created in a certain text file for reading later
 				BufferedWriter bw = null;
 				try
 				{
 					// set writer to append names to file
-					bw = new BufferedWriter(new FileWriter("tournaments/files.txt", true));
+					bw = new BufferedWriter(new FileWriter(tournamentFile, true));
 					bw.write(fileName);	// append current file name
 					bw.newLine();		// create new line for next entry
 					bw.flush();			// write to file
