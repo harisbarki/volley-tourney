16d60e0bae80544cf9de1b4648f240dcc970ef0d
Dido Maulana <dmaulana96@hotmail.com>
2016-12-02 14:25:51 -0330

    Edit teams class

--- Source_code/GUI/EditTeams.java
+++ Source_code/GUI/EditTeams.java
@@ -1,7 +1,13 @@
 package GUI;
-
 import Model.*;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+
+import java.awt.Insets;
+import java.awt.MenuBar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -9,8 +15,11 @@ import java.awt.event.MouseEvent;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
@@ -29,34 +38,42 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import javax.swing.JScrollBar;
 import javax.swing.ListSelectionModel;
 
-@SuppressWarnings("serial")
 public class EditTeams extends JPanel {
-
+	
 	private JTextField txtTeamName;
 	private JTextField txtPlayerName;
 	private ArrayList<Player> players;
 	private JList<String> lstPlayers;
 	private JSpinner selectAge;
 	private DefaultListModel<String> model;
+	
 	private Tournament tournament;
 	private Team oldTeam;
 
+	private final int WIDTH  = 750;
+	private final int HEIGHT = 450;
+	
 	/**
 	 * Create the frame.
 	 */
 	public EditTeams(Tournament tourney, Team team) {
 
 		setBorder(new EmptyBorder(5, 5, 5, 5));
-		setLayout(null);
-
+		setLayout(null);	
+		
 		this.tournament = tourney;
 		this.oldTeam = team;
+		
 
 		txtTeamName = new JTextField(team.getName());
 		txtTeamName.setBounds(120, 27, 313, 19);
@@ -84,19 +101,19 @@ public class EditTeams extends JPanel {
 		lblPlayerAge.setBounds(14, 166, 70, 15);
 		add(lblPlayerAge);
 
-		selectAge = new JSpinner(new SpinnerNumberModel(10, 0, 100, 1));
+		selectAge = new JSpinner(new SpinnerNumberModel(10,0,100,1));
 		selectAge.setSize(40, 25);
 		selectAge.setLocation(101, 166);
 		add(selectAge);
-
+		
 		players = new ArrayList<Player>();
 		model = new DefaultListModel<String>();
-
+		
 		for (Player p : team.getPlayers()) {
 			model.addElement(p.getName() + " : " + p.getAge());
 			players.add(p);
 		}
-
+		
 		JButton btnAddPlayer = new JButton("Add Player");
 		btnAddPlayer.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
@@ -104,91 +121,89 @@ public class EditTeams extends JPanel {
 				int age;
 				try {
 					age = (int) selectAge.getValue();
-					name = txtPlayerName.getText();
-
-					if (name.equals("") || !name.matches("[a-zA-Z\\s]+")) {
+					name  = txtPlayerName.getText();
+					
+					if(name.equals("") || !name.matches("[a-zA-Z\\s]+") ) {
 						throw new NullPointerException();
-					} else {
-						Player p = new Player(name, age);
+					}
+					else {
+						Player p = new 	Player(name, age);
 						players.add(p);
 						model.addElement(name + " : " + age);
 					}
-				} catch (NullPointerException ex) { // validate input
-					JOptionPane.showMessageDialog(null, "Please enter a valid name for each player", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				} finally {
+				}
+				catch (NullPointerException ex) { // validate input
+					JOptionPane.showMessageDialog(null, "Please enter a valid name for each player", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				finally {
 					txtPlayerName.setText("");
-					selectAge.setValue(10);
+					selectAge.setValue(10);					
 				}
 			}
 		});
 		btnAddPlayer.setBounds(101, 200, 114, 23);
 		add(btnAddPlayer);
-
+		
 		JButton btnEditTeam = new JButton("Save");
 		btnEditTeam.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
 				String teamName;
 				try {
 					teamName = txtTeamName.getText();
-					if (teamName.equals(""))
-						throw new NullPointerException();
-					if (players.size() < tournament.minimumTeamSize())
-						throw new IllegalStateException();
+					if (teamName.equals(""))     throw new NullPointerException();
+					if (players.size() < tournament.minimumTeamSize()) throw new IllegalStateException();
 					else {
 						Team newTeam = new Team(teamName);
-						for (Player p : players)
-							newTeam.addPlayer(p);
-						if (tournament.addTeam(newTeam)) {
+						for (Player p : players) newTeam.addPlayer(p);
+						if(tournament.addTeam(newTeam)) {
 							System.out.print(tournament.getName() + ": " + tournament.showTeams());
-
+							
 							tournament.removeTeam(oldTeam);
-							deleteTeam(tournament.getName(), oldTeam);
-							saveTeam(tournament.getName(), newTeam);
-
-							JOptionPane.showMessageDialog(null, teamName + " has been saved!", "Success!",
-									JOptionPane.INFORMATION_MESSAGE);
-						} else {
-							JOptionPane.showMessageDialog(null, "Tournament is already full!", "Error",
-									JOptionPane.ERROR_MESSAGE);
+							deleteTeam(tournament.getId() + "", oldTeam);
+							saveTeam(tournament.getId() + "", newTeam);
+							
+							JOptionPane.showMessageDialog(null,teamName+ " has been saved!", "Success!", JOptionPane.INFORMATION_MESSAGE);
 						}
-					}
-				} catch (IllegalStateException i) {
+						else {
+							JOptionPane.showMessageDialog(null, "Tournament is already full!", "Error", JOptionPane.ERROR_MESSAGE);
+						}							
+					}	
+				}
+				catch(IllegalStateException i) {
 					JOptionPane.showMessageDialog(null, "Please add more players", "Error", JOptionPane.ERROR_MESSAGE);
-				} catch (NullPointerException n) {
-					JOptionPane.showMessageDialog(null, "Please set a name for the team", "Error",
-							JOptionPane.ERROR_MESSAGE);
+				}
+				catch(NullPointerException n) {
+					JOptionPane.showMessageDialog(null, "Please set a name for the team", "Error", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 		btnEditTeam.setBounds(36, 251, 150, 23);
 		add(btnEditTeam);
-
+		
 		JButton btnRemovePlayer = new JButton("Remove Player");
 		btnRemovePlayer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String player;
 				try {
-					if (lstPlayers.getModel().getSize() == 0)
-						throw new IndexOutOfBoundsException();
+					if(lstPlayers.getModel().getSize() == 0) throw new IndexOutOfBoundsException();
 					player = lstPlayers.getSelectedValue();
 					player = player.substring(0, player.indexOf(':') - 1);
-					for (Player p : players)
-						if (p.getName() == player)
-							players.remove(p);
-					model.remove(lstPlayers.getSelectedIndex());
-				} catch (IndexOutOfBoundsException n) { // handle empty list
-					JOptionPane.showMessageDialog(null, "There are no players to remove", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				} catch (NullPointerException n) { // handle no player selected
-					JOptionPane.showMessageDialog(null, "Please select a player to remove", "Error",
-							JOptionPane.ERROR_MESSAGE);
+					for (int i = 0; i < players.size(); i++)
+						if (players.get(i).getName().equals(player))
+							players.remove(players.get(i));
+					model.remove(lstPlayers.getSelectedIndex());					
+				}
+				catch(IndexOutOfBoundsException n) { // handle empty list
+					JOptionPane.showMessageDialog(null, "There are no players to remove", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				catch(NullPointerException n) { // handle no player selected
+					JOptionPane.showMessageDialog(null, "Please select a player to remove", "Error", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 		btnRemovePlayer.setBounds(300, 251, 186, 23);
 		add(btnRemovePlayer);
-
+		
 		JLabel lblTournament = new JLabel("Tournament:");
 		lblTournament.setBounds(14, 62, 150, 14);
 		add(lblTournament);
@@ -196,7 +211,7 @@ public class EditTeams extends JPanel {
 		JLabel tmntLbl = new JLabel(tournament.getName());
 		tmntLbl.setBounds(120, 62, 500, 15);
 		add(tmntLbl);
-
+		
 		JScrollPane sp = new JScrollPane();
 		lstPlayers = new JList<String>();
 		lstPlayers.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -204,49 +219,107 @@ public class EditTeams extends JPanel {
 		sp.setViewportView(lstPlayers);
 		add(sp);
 		sp.setBounds(300, 61, 186, 169);
-
+	
 	}
-
+	
 	public void deleteTeam(String name, Team t) {
-
-	}
-
-	public void saveTeam(String name, Team t) {
 		// get name for retrieving save file
 		String fileName = "tournaments/" + name + ".xml";
-
+				
 		// xml document for to load file
 		Document dom;
-
+						
 		// for building document builder
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-		try {
+		DocumentBuilderFactory dbf =  DocumentBuilderFactory.newInstance();
+						
+		try
+		{
 			// create document using document builder
 			DocumentBuilder db = dbf.newDocumentBuilder();
-
+							
 			// load the xml file
 			dom = db.parse(fileName);
-
+					
+			// get root element
+			Element root = dom.getDocumentElement();
+			
+			NodeList tlNodeList = root.getElementsByTagName("teamList");
+			Element teamList = (Element)tlNodeList.item(0);
+			
+			NodeList teamNodeList = teamList.getElementsByTagName("team");
+			
+			for (int i = 0; i < teamNodeList.getLength(); i++) {
+				
+				Element teamElement = (Element) teamNodeList.item(i);
+				Node teamNode = teamNodeList.item(i);
+				
+				NodeList tnNodeList = teamElement.getElementsByTagName("teamName");
+				Element tnElement = (Element)tnNodeList.item(i);
+				
+				System.out.println("\nXML : " + tnElement.getTextContent());
+				System.out.println("Team : " + t.getName());
+				
+				if (tnElement.getTextContent().equals(t.getName())) {
+					tlNodeList.item(i).removeChild(teamNode);
+					System.out.println("team deleted");
+					break;
+				}
+			}
+		}
+		catch(ParserConfigurationException pce)
+		{
+			System.out.println(pce.getMessage());
+		}
+		catch(SAXException se)
+		{
+			System.out.println(se.getMessage());
+		}
+		catch(IOException ioe)
+		{
+			System.out.println(ioe.getMessage());
+		}
+	}
+	
+	public void saveTeam(String name, Team t)
+	{	
+		// get name for retrieving save file
+		String fileName = "tournaments/" + name + ".xml";
+		
+		// xml document for to load file
+		Document dom;
+				
+		// for building document builder
+		DocumentBuilderFactory dbf =  DocumentBuilderFactory.newInstance();
+				
+		try
+		{
+			// create document using document builder
+			DocumentBuilder db = dbf.newDocumentBuilder();
+					
+			// load the xml file
+			dom = db.parse(fileName);
+			
 			// get root element
 			Element root = dom.getDocumentElement();
 			NodeList teamList = root.getElementsByTagName("teamList");
-
+			
 			Element tl = (Element) teamList.item(0);
 			Element team = dom.createElement("team");
-
+		
 			Element s = dom.createElement("teamName");
 			s.appendChild(dom.createTextNode(t.getName()));
 			team.appendChild(s);
+				
 
 			ArrayList<Player> players = t.getPlayers();
-
+			
 			// for each player create node and append name and age for each
-			for (int i = 0; i < players.size(); i++) {
+			for(int i = 0; i < players.size(); i++)
+			{
 				Element p = dom.createElement("player");
 				Element n = dom.createElement("name");
 				Element a = dom.createElement("age");
-
+					
 				n.appendChild(dom.createTextNode(players.get(i).getName()));
 				a.appendChild(dom.createTextNode("" + players.get(i).getAge()));
 				p.appendChild(n);
@@ -254,8 +327,9 @@ public class EditTeams extends JPanel {
 				team.appendChild(p);
 			}
 			tl.appendChild(team);
-
-			try {
+			
+			try
+			{
 				// add properties to file and format it
 				Transformer tr = TransformerFactory.newInstance().newTransformer();
 				tr.setOutputProperty(OutputKeys.INDENT, "yes");
@@ -263,19 +337,29 @@ public class EditTeams extends JPanel {
 				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 				tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "tournament.dtd");
 				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
+				
 				// save file
 				tr.transform(new DOMSource(dom), new StreamResult(new FileOutputStream(fileName)));
-			} catch (TransformerException te) {
+			}
+			catch(TransformerException te)
+			{
 				System.out.println(te.getMessage());
-			} catch (IOException ioe) {
+			}
+			catch(IOException ioe)
+			{
 				System.out.println(ioe.getMessage());
 			}
-		} catch (ParserConfigurationException pce) {
+		}
+		catch(ParserConfigurationException pce)
+		{
 			System.out.println(pce.getMessage());
-		} catch (SAXException se) {
-			System.out.println(se.getMessage());
-		} catch (IOException ioe) {
+		}
+		catch(SAXException se)
+		{
+            System.out.println(se.getMessage());
+        }
+		catch(IOException ioe)
+		{
 			System.out.println(ioe.getMessage());
 		}
 	}
